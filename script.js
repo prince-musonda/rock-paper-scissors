77bcf61ac0012a9cdcbba8b54130fc7b2402c066
Prince Musonda <115486288+prince-musonda@users.noreply.github.com>
2022-11-30 11:32:04 +0200

    Edit scipt.js
    removed the code that played the game 5 times.
    Also removed any 'console.log' statements from the file.
    Added new code for the manupilation the dom or the  UI elements inserted in 'index.html' so as to give the game a GUI interface rather than playing the game through the console.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function getComputerChoice(){
-    randomNumber = Math.floor(Math.random() * 3)
-    choiceList = ['rock','paper','scissors']
-    computerChoice = choiceList[randomNumber]
+    const randomNumber = Math.floor(Math.random() * 3)
+    const choiceList = ['rock','paper','scissors']
+    const computerChoice = choiceList[randomNumber]
     return computerChoice
 }
 
@@ -40,42 +40,83 @@ function playRound(playerSelection,computerSelection){
 
 }
 
-function game(){
-    let playerScore = 0
-    let computerScore = 0
-    let message = ''
-    for(i = 1; i <= 5; i ++){
-        //play the game 5 times in a raw and see whole wins
-        let playerSelection = prompt('rock paper or scissors')
-        let computerSelection = getComputerChoice()
-        let results = playRound(playerSelection,computerSelection)
-        console.log(playerSelection.toLowerCase() + ' vs ' + computerSelection)
+const rockButton = document.querySelector('#rock');
+const paperButton = document.querySelector('#paper');
+const scissorsButton = document.querySelector('#scissors');
+const userScoreDisplay = document.querySelector('#user-score');
+const computerScoreDisplay = document.querySelector('#computer-score');
+const gameStatics = document.querySelector('#choices-display')
+const theWinnerAnnoucePanel = document.createElement('span')
 
-        if (results == 'win' ){
-            console.log(`you win ${playerSelection.toLowerCase()} beats ${computerSelection}`)
-            playerScore += 1
-        }
-        else if(results == 'lose'){
-            console.log(`you lose ${computerSelection} beats ${playerSelection.toLowerCase()}`)
-            computerScore += 1
-        }
-    }
+let userScore  = 0;
+let computerScore = 0;
+let roundsPlayed = 0;
 
-    //game over
-    // summarize the results for the game
-    console.log('game over')
-    if (playerScore > computerScore){
-        message = `YOUR SCORE: ${playerScore}
-COMPUTER : ${computerScore}
-        YOU WIN, CONGRATULATIONS`
-    }else if(computerScore > playerScore){
-        message = `YOUR SCORE: ${playerScore}
-COMPUTER : ${computerScore}
-        YOU LOSE, The Computer Won`
-    }else{
-        message = "It IS A TIE, NOBODY WON"
-    }
-    console.log(message)
+function announceTheWinner(winner){
+     //clearing the results panel
+     let ResultsToBeRemoved = gameStatics.children
+     ResultsToBeRemoved = [...ResultsToBeRemoved]
+     ResultsToBeRemoved.forEach(result => {result.remove()})
+     //announcing the winner
+     if (winner == 'user') theWinnerAnnoucePanel.textContent = `You Win!!!` 
+     else if(winner == 'computer') theWinnerAnnoucePanel.textContent = `The Computer has Won!!!`
+     theWinnerAnnoucePanel.setAttribute('style','color:red; font-size:50px;text-align:center')
+     gameStatics.appendChild(theWinnerAnnoucePanel)
+     
+     //reset the scores
+     computerScore = 0;
+     computerScoreDisplay.textContent = 0;
+     userScore = 0
+     userScoreDisplay.textContent = 0
+     
 }
 
-game()
+
+
+function playGame(e){
+    if (roundsPlayed < 5){
+        // removing the winner anounce panel incase it is there
+            roundsPlayed += 1;
+            const userChoice = e.target.textContent;
+            const computerChoice = getComputerChoice();
+            const miniFlexContainer = document.createElement('div')
+            miniFlexContainer.setAttribute('class','mini-flex-conatiner')
+            const computerChoiceAsFlexItem = document.createElement('p');
+            const userChoiceAsFlexItem = document.createElement('p')
+            computerChoiceAsFlexItem.textContent = computerChoice;
+            computerChoiceAsFlexItem.setAttribute('class','info')
+            userChoiceAsFlexItem.textContent = userChoice;
+            userChoiceAsFlexItem.setAttribute('class','info')
+            miniFlexContainer.setAttribute('style','display:flex; justify-content:space-between')
+            miniFlexContainer.appendChild(computerChoiceAsFlexItem)
+            miniFlexContainer.appendChild(userChoiceAsFlexItem)
+            gameStatics.appendChild(miniFlexContainer)
+            win_status = playRound(userChoice,computerChoice)
+            if (win_status == 'win'){
+                userScore += 1;
+                userScoreDisplay.textContent = userScore
+            }else if(win_status == 'lose'){
+                computerScore += 1;
+            computerScoreDisplay.textContent = computerScore
+            }
+
+            //check who as reached 5 points
+            if(computerScore == 5 && userScore < 5){
+                announceTheWinner('computer')
+                gameStatics.setAttribute 
+            }else if(userScore == 5 && computerScore < 5){
+                announceTheWinner('user')
+            }
+        }
+    // reset or clear the outcomes panel for every five rounds played
+    else if(roundsPlayed >= 5){
+        roundsPlayed = 0;
+        let ResultsToBeRemoved = gameStatics.children
+        ResultsToBeRemoved = [...ResultsToBeRemoved]
+        ResultsToBeRemoved.forEach(result => {result.remove()})
+    }
+}
+
+rockButton.addEventListener('click',playGame)
+paperButton.addEventListener('click',playGame)
+scissorsButton.addEventListener('click',playGame)
